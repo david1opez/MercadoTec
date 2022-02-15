14fc541f732f1ef4ff19b64f3b5adc9b32ac2f7e
david <degl0412@gmail.com>
2022-02-15 14:28:08 -0600

    Chenged style condition order for cleaner code

--- components/FeaturedPost.tsx
+++ components/FeaturedPost.tsx
@@ -7,7 +7,7 @@ import{ colors } from "../StyleVariables";
 
 const FeaturedPost = ({index}: {index: number}) => {
   return (
-    <TouchableOpacity style={index != 0 ? styles.container : [styles.container, {marginLeft: s(15)}]}>
+    <TouchableOpacity style={index == 0 ? [styles.container, {marginLeft: s(15)}] : styles.container}>
 
       <Image style={styles.image} source={{uri: "https://scontent.fgdl10-1.fna.fbcdn.net/v/t39.30808-6/p720x720/273988752_4799358050151232_2951886400619287815_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=5cd70e&_nc_eui2=AeFN19E_M2O7UBqM3Mxiw7RIf_WCfT_pbKV_9YJ9P-lspdSbyGg4yP0MUxklUGxeoillZ_ql9H0kDMFb1F06he6T&_nc_ohc=9MxFFenXNNMAX9ja2gl&_nc_ht=scontent.fgdl10-1.fna&oh=00_AT9R4xMUojA0YeoSVtcu-OW-uXig_pPf-NqIA8iQy2nP3A&oe=621084E6"}} />
       
